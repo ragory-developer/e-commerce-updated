7ec9c36877fff73eb278a2dbb7aac93d87c6bad6
Shaharia Hossain <shahariahossain72@gmail.com>
2026-03-01 19:49:29 +0600

    make changes in nest js so push

--- src/main.ts
+++ src/main.ts
@@ -53,6 +53,6 @@ async function bootstrap() {
 }
 
 bootstrap().catch((err) => {
-  console.error('❌ Application failed to start', err);
+  console.error('❌ Application do start', err);
   process.exit(1);
 });
